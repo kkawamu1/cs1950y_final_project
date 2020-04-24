e8bf21bb96412926032c37ad0c5d6f07ac080973
kkawamu1 <58097201+kkawamu1@users.noreply.github.com>
2020-04-24 14:53:56 +0900

    basic logic for naive implementation
    I added some basic stuff for naive implementation. We still need to do the fraction to do the full naive version.

--- pagerank.rkt
+++ pagerank.rkt
@@ -3,45 +3,80 @@
 --sig for pages
 sig Page {}
 
+---------- States ----------
 
 --sig for state, pageRank is a relation from Page to Int (each page get to have its rank), link is a set of edges between the pages (directed)
 sig State {
-    pageRank: set Page -> Int
+    pageRank: set Page -> Int,
     link: set Page -> Page
 }
 
 
---pred: a set of states is a set containing all the iterations and an initial state.
-pred abstractState {
-      State = Iteration1 + Iteration2 + Iteration3 + Iteration4 + Iteration5     
+---------- Events ----------
+
+sig Event {
+    pre: one State,
+    post: one State
+}
+
+state[State] initState {
+    -- constraints for the first state
+    -- Fill me in!
+    
+--we have to decide what value each page should hold as its initial value. In the original implementation, the initial value for a page p = 1 / the total numebr of pages.
+
+---******we need to increase the bounds for int; otherwise, we can only have [-7, 8]
+    pageRank = Page -> sing[1]
+    link = Page -> Page
+}
+
+---run {} for exactly 4 Page, exactly 3 State, exactly 4 Int
+
+-- here maybe define the final state as the bad state that we don't want the algorithm to reach. Then we check if it is possible to get to this final state in x amount of transitions.
+--or just define this final state as some random sate, which does not constrain anything, but there for the sake of trace.
+state[State] finalState {
+    -- Fill me in!
+    link = Page -> Page
 }
 
 
+transition[State] naiveUpdate[e: Event] {
+    -- constraints for how the ranks should be distributed
+    -- relating current state s, next state s', and event between them e
+    -- Fill me in
+    this = e.pre
+    this' = e.post
 
---pred: no change in the pages themselves across all the iterations. No new pages or no disappearing pages.
-pred abstractPage{
+    ---boat = Near implies (boat' = Far and e.toMove in near and near' = near - e.toMove and far' = far + e.toMove)
+    ---boat = Far implies (boat' = Near and e.toMove in far and near' = near + e.toMove and far' = far - e.toMove)
+    ---new page rank for v = sum over the vertices, u,  with incoming edge to v: [u's old page rank / |u's outgoing edges|] 
+    --pageRank: set Page -> Int,
+    --link: set Page -> Page
 
+    --link will stay the same between the states
+    link' = link
+
+    --All the pages stay the same
+    pageRank'.Int = pageRank.Int
+    
+    ---*****We have to be careful when taking sum because if we blindly take the sum, we can mess up since there could be duplicates in what we are summing over.*****----
+    --https://github.com/cemcutting/Forge/blob/docs/forge/docs/basicForgeDocumentation.md#integers
+
+    --for each vertex v, the value of v in the next iteration is the sum of the ranks / # outgoing edges over the vertex u where u->v is in link 
+    all vNext : pageRank.Int| vNext.pageRank' = sing[(sum incoming: link.vNext | {sum[incoming.pageRank] })]
+    
 }
 
---pred: all the pages need to have their associated ranks.
-pred allPageHasRank {
-
-}
---pred: no change in the edges across all the iterations. No new edges or no disappearing edges.
-pred abstractEdge{
-
+transition[State] naiveAlgorithm {
+    some e: Event | naiveUpdate[this, this', e]
 }
 
+trace<|State, initState, naiveAlgorithm, finalState|> traces: linear {}
+
+run<|traces|> for 2 State, exactly 3 Event, 4 Int
 
 
---pred for general setup
-pred setup {
-    abstractState
-    abstractPage
-    abstractEdge
-}
-
-
+----------Assertion --------------
 pred noZeroRank[pagerank: set Page->Int] {
     -- constraints for no page having zero rank
 }
@@ -54,46 +89,6 @@ pred neverZeroRank {
         noZeroRank[s.pageRank]
     }
 }
-
-sig Event {
-    pre: one State,
-    post: one State,
-}
-
-state[State] initState {
-    -- constraints for the first state
-    -- Fill me in!
-    
---we have to decide what value each page should hold as its initial value. In the original implementation, the initial value for a page p = 1 / the total numebr of pages.
-    pageRank: set Page -> Int
-    link = Page -> Page
-}
-
-
--- here maybe define the final state as the bad state that we don't want the algorithm to reach. Then we check if it is possible to get to this final state in x amount of transitions.
-state[State] finalState {
-    -- Fill me in!
-    
-
-}
-transition[State] crossRiver[e: Event] {
-    -- constraints for how the ranks should be distributed
-    -- relating current state s, next state s', and event between them e
-    -- Fill me in
-    this = e.pre
-    this' = e.post
-
-    ---boat = Near implies (boat' = Far and e.toMove in near and near' = near - e.toMove and far' = far + e.toMove)
-    ---boat = Far implies (boat' = Near and e.toMove in far and near' = near + e.toMove and far' = far - e.toMove)
-}
-
-transition[State] puzzle {
-    some e: Event | crossRiver[this, this', e]
-}
-
-trace<|State, initState, puzzle, finalState|> traces: linear {}
-
-run<|traces|> neverStealing for 12 State, exactly 11 Event, 6 Animal, exactly 3 Pet, exactly 3 Owner, 2 Position, 4 Int
 
 
 
